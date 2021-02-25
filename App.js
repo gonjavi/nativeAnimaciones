07d263b218f7e5b38b0e29cf83e4ccc52920c558
manci <gmancillapaz@gmail.com>
2021-02-25 12:09:34 -0500

    animacion en secuencia spring y timing

--- App.js
+++ App.js
@@ -12,13 +12,14 @@ import Animacion3 from './components/Animacion3';
 import Animacion4 from './components/Animacion4';
 import Animacion5 from './components/Animacion5';
 import Animacion6 from './components/Animacion6';
+import Animacion7 from './components/Animacion7';
 
 const App = () => {
   return (
     <>
       <View style={styles.contenido}>
         
-        <Animacion6 />
+        <Animacion7 />
       </View>
      
     </>
