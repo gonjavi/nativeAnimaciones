5964279d7168d63a02eb8b348f5219a92329be53
manci <gmancillapaz@gmail.com>
2021-02-25 09:42:15 -0500

    instalacion proyecto

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.contenido}>
+
+      </View>
+     
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenido: {
+    marginTop: 100,
+  }
+});
+
+export default App;
